5745bcdd421a2415a4c9da34a96e42981cff7364
hloth <gitlab@hloth.dev>
2023-01-17 21:11:39 +0400

    MUI does not work with Next.js /app directory

---
+++ src/ssr/mui/ThemeWrapper.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '/src/ssr/mui/createEmotionCache'
+import theme from '/src/ssr/mui/theme'
+
+const cache = createEmotionCache()
+
+export default function ThemeWrapper(props: { children: React.ReactNode }) {
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
---
+++ src/ui/fragments/Navigation/index.tsx
@@ -0,0 +1,7 @@
+import AppBar from './AppBar'
+
+export default function Navigation() {
+  return (
+    <AppBar />
+  )
+}
