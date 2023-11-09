bc87252a7f977cfe38c32ab995a55f1e2ab7bcfe
hloth <gitlab@hloth.dev>
2023-11-09 22:39:59 +0400

    Update next.js to latest, remove MUI, add shadcn (radix ui) components, moving to FSD

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,6 @@
-import './globals.css'
+import '@/shared/styles/tailwind.css'
 import type { AppProps } from 'next/app'
-import ThemeWrapper from '/src/ssr/mui/ThemeWrapper'
-import { LocalizationProvider as MUILocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { ru } from 'date-fns/locale'
-import ApolloClient from '/src/api/ApolloClient'
-import { appWithTranslation } from 'next-i18next'
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeWrapper>
-      <MUILocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ru}>
-        <ApolloClient>
-          <Component {...pageProps} />
-        </ApolloClient>
-      </MUILocalizationProvider>
-    </ThemeWrapper>
-  )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-
-export default appWithTranslation(App)
